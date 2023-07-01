05185c20b3fa963f56e69a9d16da2dcb8f0eff8c
Jyoti <js10092000@gmail.com>
2023-07-01 08:24:26 +0530

    alerts added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 import { useState } from 'react';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500); 
+  }
+
   const toggleMode = () => {
     if(mode === 'dark') {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+
     } else {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode has been enabled", "success");
     }
   }
   return (
     <>
     <Navbar mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container">
-    <TextForm heading="Enter the text to modify below" mode={mode}/> 
+    <TextForm heading="Enter the text to modify below" mode={mode} showAlert={showAlert}/> 
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,6 +4,7 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to uppercase!", "success");
     }
 
     const handleOnChange = (event) => {
@@ -13,22 +14,26 @@ export default function TextForm(props) {
     const handleLowClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to lowercase!", "success");
     }
 
     const handleDelete = () => {
         let newText = "";
         setText(newText);
+        props.showAlert("Text deleted!", "success");
     }
 
     const handleCopy = () => {
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard!", "success");
     }
 
     const handleExtraSpace = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Extra spaces deleted!", "success");
     }
 
     const [text, setText] = useState('');
