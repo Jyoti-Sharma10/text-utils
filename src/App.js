74879d8c0d69b6817d5aa250573ba69014ef62e6
Jyoti <js10092000@gmail.com>
2023-07-04 16:24:07 +0530

    router removed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import { useState } from 'react';
-import About from './components/About';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -34,29 +32,12 @@ function App() {
   }
   return (
     <>
-    <BrowserRouter basename='/text-utils'>
-    <Routes>
-      <Route exact path='/text-utils' element={<>
-        <Navbar mode={mode} toggleMode={toggleMode}/>
-        <Alert alert={alert}/>
-        <div className="container">
-        <TextForm heading="Enter the text to modify below" mode={mode} showAlert={showAlert}/>
-        </div>
-        </>
-    }/>
-    
-     <Route path='/about' element={
-       <>
-       <Navbar mode={mode} toggleMode={toggleMode}/>
-       <Alert alert={alert}/>
-       <div className="container">
-         <About mode={mode}/>
-       </div>
-       </>
-     }/>
-     </Routes>
-     </BrowserRouter>
-     </>
+    <Navbar mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container">
+    <TextForm heading="Enter the text to modify below" mode={mode} showAlert={showAlert}/>
+    </div>
+    </>
   );
 }
 
