05289012da1868c9b2a00fea6a6cffee64c1e901
Jyoti <js10092000@gmail.com>
2023-07-04 10:34:55 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import { useState } from 'react';
+import About from './components/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -23,7 +25,7 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-
+    
     } else {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
@@ -32,12 +34,29 @@ function App() {
   }
   return (
     <>
-    <Navbar mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert}/>
-    <div className="container">
-    <TextForm heading="Enter the text to modify below" mode={mode} showAlert={showAlert}/> 
-    </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<>
+        <Navbar mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <div className="container">
+        <TextForm heading="Enter the text to modify below" mode={mode} showAlert={showAlert}/>
+        </div>
+        </>
+    }/>
+    
+     <Route path='/about' element={
+       <>
+       <Navbar mode={mode} toggleMode={toggleMode}/>
+       <Alert alert={alert}/>
+       <div className="container">
+         <About mode={mode}/>
+       </div>
+       </>
+     }/>
+     </Routes>
+     </BrowserRouter>
+     </>
   );
 }
 
